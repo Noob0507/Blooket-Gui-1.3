04f7c9aee176379cbc941e26cf1f708c4f28f549
Aerell <aerellmck@gmail.com>
2023-02-16 19:02:19 -0600

    Fix nested scripts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelector('.categories').innerHTML = "Loading cheats...";
 await fetch("https://raw.githubusercontent.com/Minesraft2/Blooket-Cheats/main/unobfuscated/Bookmarklets.html").then(async x => document.querySelector("#unobfuscated").contentDocument.write(await x.text().then(x => x.slice(0, x.indexOf("<style")))));
 const unobfuscated = parseCheats(document.querySelector("#unobfuscated"));
+window.a = unobfuscated
 
 document.querySelector('.categories').innerHTML = "";
 const icons = parseIcons(document.querySelector("#unobfuscated"));
@@ -10,55 +11,47 @@ loadPage();
 window.onpopstate = loadPage;
 function loadPage() {
     document.querySelector('.categories').innerHTML = "";
-    const hash = decodeURI(location.hash.replace("#", ""));
-    document.querySelector('h1').innerHTML = `${hash || "Blooket"} Cheats${hash ? `<span style="font-size: 1rem; display: block">(Click a cheat to copy the script or drag into bookmarks bar)</span>` : ""}`;
-    if (hash) {
-        document.querySelector('.categories').innerHTML = `<a style="width: 170px;" href="#" class="category"><img style="width: 100%;" title="Back" alt="Back" src="${icons.Default}"><div class="cheatlabel" style="background-color: #24b6ff;">Back</div></a>`;
-        for (const key in unobfuscated[hash]) {
-            const category = unobfuscated[hash][key];
-            const categoryElement = document.createElement("a");
-            categoryElement.style.width = "170px"
-            categoryElement.href = category;
-            categoryElement.classList.add("category");
-            categoryElement.onclick = () => navigator.clipboard.writeText(category);
-            const img = document.createElement("img");
-            img.title = img.alt = key;
-            img.src = icons[hash];
-            img.style.width = "100%";
-            categoryElement.append(img);
-            const label = document.createElement("div");
-            label.classList.add("cheatlabel");
-            label.innerText = key;
-            categoryElement.append(label);
-            document.querySelector(".categories").appendChild(categoryElement);
-        }
+    const hash = decodeURI(location.hash.slice(1));
+    const parent = hash.split("/").reduce((a, b) => a[b], unobfuscated);
+    if (parent) {
+        if (!parent) return window.location.hash = "";
+        document.querySelector('h1').innerHTML = `${hash} Cheats<span style="font-size: 1rem; display: block">(Click a cheat to copy the script or drag into bookmarks bar)</span>`;
+        document.querySelector('.categories').innerHTML = `<a style="width: 170px;" href="#${hash.split('/').slice(0, -1).join('/')}" class="category"><img style="width: 100%;" title="Back" alt="Back" src="${icons.Default}"><div class="cheatlabel" style="background-color: #24b6ff;">Back</div></a>`;
+        for (const key in parent) createButton(key, parent, hash.split("/")[0]);
     } else {
-        for (const key in unobfuscated) {
-            const categoryElement = document.createElement("a");
-            categoryElement.style.width = "135px"
-            categoryElement.href = `#${key}`;
-            categoryElement.classList.add("category");
-            const img = document.createElement("img");
-            img.title = key;
-            img.src = icons[key];
-            img.style.width = "100%";
-            categoryElement.append(img);
-            const label = document.createElement("div");
-            label.classList.add("label");
-            label.innerText = key;
-            categoryElement.append(label);
-            document.querySelector(".categories").appendChild(categoryElement);
-        }
+        document.querySelector('h1').innerHTML = "Blooket Cheats";
+        for (const key in unobfuscated) createButton(key, unobfuscated, typeof unobfuscated[key] == "string" && "Default");
     }
 }
 
+function createButton(key, parent, hash) {
+    const category = hash && parent[key];
+    const categoryElement = document.createElement("a");
+    categoryElement.style.width = hash ? "135px" : "135px";
+    categoryElement.href = hash ? typeof category == "string" ? category : `#${hash}/${key}` : "#" + key;
+    categoryElement.classList.add("category");
+    hash && (categoryElement.onclick = () => navigator.clipboard.writeText(category));
+    const img = document.createElement("img");
+    img.title = img.alt = key;
+    img.src = icons[hash || key];
+    img.style.width = "100%";
+    categoryElement.append(img);
+    const label = document.createElement("div");
+    label.classList.add(hash && typeof category == "string" ? "label" : "label");
+    label.style.backgroundColor = hash && typeof category == "string" ? "#b624ff" : "#ff7b24";
+    label.innerText = key;
+    categoryElement.append(label);
+    document.querySelector(".categories").appendChild(categoryElement);
+}
+
 function parseCheats(iframe) {
-    let obj = {};
-    for (const child of iframe.contentDocument.querySelector('dl').children) {
-        let category = obj[(child.querySelector("dl") || child).parentNode.querySelector(":scope > h3")?.innerHTML || "Default"] ||= {};
-        for (const { innerHTML, href } of child.querySelectorAll("a")) category[innerHTML] = decodeURI(href);
-    }
-    return obj
+    return parseDL(iframe.contentDocument.querySelector("dl"));
+    // let obj = {};
+    // for (const child of iframe.contentDocument.querySelector('dl').children) {
+    //     let category = obj[(child.querySelector("dl") || child).parentNode.querySelector(":scope > h3")?.innerHTML || "Default"] ||= {};
+    //     for (const { innerHTML, href } of child.querySelectorAll("a")) category[innerHTML] = decodeURI(href);
+    // }
+    // return obj
 }
 
 function parseIcons(iframe) {
@@ -66,4 +59,15 @@ function parseIcons(iframe) {
     for (const child of iframe.contentDocument.querySelector('dl').children)
         obj[(child.querySelector("dl") || child).parentNode.querySelector(":scope > h3")?.innerHTML || "Default"] = child.querySelector("a").getAttribute("icon");
     return obj
+}
+
+// Parsing Bookmarks.html files
+function parseDL(dl) {
+    return Array.from(dl.children).map(dt => {
+        if (dt.querySelector('h3')) return [dt.querySelector("h3").innerText, parseDL(dt.querySelector("dl"))];
+        return [dt.innerText, decodeURI(dt.querySelector('a').href)];
+    }).reduce(function reduce(obj, [key, value]) {
+        obj[key] = Array.isArray(value) ? value.reduce(reduce, {}) : value;
+        return obj;
+    }, {});
 }
